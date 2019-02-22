d5d5a35229b985304a75abd081b4cf22241be296
Israel Cadorin <37721806+israelmurilocadorin@users.noreply.github.com>
2019-02-22 20:26:29 -0300

    Update Fatura.java

--- ListaDeExercicios/Fatura/Fatura.java
+++ ListaDeExercicios/Fatura/Fatura.java
@@ -1,5 +1,14 @@
 public class Fatura{
-
+    
+    private String codigo;
+    private String descricao;
+    private Integer quantidade;
+    private double preco;
+    
+    public Fatura(){
+    
+    }
+    
     public double Calcular (double preco, int quantidade){
         double resultado = preco * quantidade;
         return resultado;

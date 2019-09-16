fc0e42a7203be0a4482ce7e0704274296230443c
israelmurilocadorin <israel.cadorin@catolicasc.org.br>
2019-09-16 17:24:37 -0300

    Novos Exercícios

---
+++ adivinharNumero/TesteAdivinhar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TesteAdivinhar{
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+        Random rand = new Random();
+        int numero = 0 + random.nextInt(999);
+        int palpite = 0;
+        int tentativas;
+
+        do{
+            System.out.print("Digite um número: ");
+            palpite = ler.nextInt();
+
+        }while(numero != palpite)
+    }
+}
+
+//https://4c26faa5-a-62cb3a1a-s-sites.googlegroups.com/site/profnewtonjava/exerciciosresolvidos/Ex02_C%C3%B3digo_Java_TOTAL_2017-2.jpg?attachauth=ANoY7com_ZEHYcy04bTTS_OrD6G1WhiY8t_N4JgWWnnXZGMj3yyj6tYhojw1S7VtFYKquFDkz7C1rB_4SR98Ne3LZ_2aqQ2b-CHzo6jxuIfU2h8nZV9MD97sItwJuP9I8JMH0SogGmnjiRQKyRmf5ATt9GsBcbeexrA3MAuidq0ReWfFIKSP_0Km8ba_yc3rXkn88NlZgNouAN0yRA5oyj3pvSXB0aB14NtdGfJf5x81Yd4VlHoakeMw2Ol6Nw7HwlGe0grGIH7IVngIA19LW8wx7EEb3J0C0A%3D%3D&attredirects=0
